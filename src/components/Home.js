817f917fc70af1556b16bf6de85e6917aae36f45
Dmitry Zenin <dzendmitry@gmail.com>
2024-10-29 00:11:42 +0400

    replacing Dmitrii to Dmitry

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,7 +20,7 @@ export default function Home() {
             bg="white"
             borderRadius={10}
             boxShadow="3px 3px 10px grey">
-                <Avatar name='Dmitrii Zenin' size="2xl" src={require('../img/avatar.jpg')} />
+                <Avatar name='Dmitry Zenin' size="2xl" src={require('../img/avatar.jpg')} />
                 <VStack alignItems="left">
                     <Heading as="h3" size="lg" color="#454545" pb="2">Dmitry Zenin</Heading>
                     <Text align="justify" maxW={1024}>I’m a dedicated <b>Engineering Team Lead</b> with a strong passion for building scalable solutions, team leadership, and continuous learning in emerging tech. My journey through diverse roles has shaped me into a well-rounded leader and developer, skilled in <b>Golang</b><FontAwesomeIcon icon={faGolang} style={{color: "#3f99de"}} size="lg"/> and experienced in creating <b>high-performance backend systems</b><FontAwesomeIcon icon={faServer} style={{color: "#36ab53"}} size="lg"/>.</Text>
